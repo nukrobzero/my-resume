d4b9906ef767ccfc23adf39a6d238b942b16bdd2
nukrobzero <nukrobzero@hotmail.com>
2023-10-12 22:55:23 +0700

    add: motions

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,6 +2,8 @@
 
 import { FC, ReactNode } from "react";
 import NavBar from "./navBar";
+import PageWrapper from "./page-wrapper";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface LayoutProps {
   children: ReactNode;
@@ -11,8 +13,19 @@ const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="max-w-screen-2xl mx-auto">
       <div className="flex flex-col lg:flex-row min-h-screen text-neutral-100">
-        <NavBar />
-        <div className="container lg:w-[70%] mx-auto">{children}</div>
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            transition={{ delay: 0.5 }}
+          >
+            <NavBar />
+          </motion.div>
+        </AnimatePresence>
+        <div className="container lg:w-[70%] mx-auto">
+          <PageWrapper>{children}</PageWrapper>
+        </div>
       </div>
     </div>
   );
