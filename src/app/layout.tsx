2842fa57536843d0c6e8efeb4988ae93944200bd
Peeradon Chairattanakumrod <peeradon.c@grpasc.com>
2024-09-06 21:01:03 +0700

    fix: code and upgrade version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "src/styles/globals.css";
+import "../styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "@/lib/providers";
--- src/components/work.tsx
+++ src/components/work.tsx
@@ -1,20 +1,44 @@
-import Pagination from "./paginations";
+'use client';
+
+import { useEffect, useState } from 'react';
+import Pagination from './paginations';
 
 export const revalidate = 60;
 
-const Work = async () => {
-  const getRepo = await fetch(`https://api.github.com/user/repos`, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
-    },
-    cache: "no-cache",
-  });
-  const res = await getRepo.json();
-  const sortedRepos = res.sort((a: any, b: any) => b.id - a.id);
+const Work = () => {
+  const [repos, setRepos] = useState<any[]>([]); // เก็บผลลัพธ์จาก API
+  const [loading, setLoading] = useState(true); // จัดการสถานะโหลดข้อมูล
+
+  useEffect(() => {
+    const callapi = async () => {
+      try {
+        const getRepo = await fetch(`https://api.github.com/user/repos`, {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+          },
+          cache: 'no-cache',
+        });
+        const res = await getRepo.json();
+        const sortedRepos = res.sort((a: any, b: any) => b.id - a.id);
+        setRepos(sortedRepos); // เก็บค่าที่เรียงแล้วใน state
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false); // ตั้งค่า loading เป็น false เมื่อโหลดเสร็จ
+      }
+    };
+
+    callapi(); // เรียกใช้ฟังก์ชันเมื่อ component ถูก mount
+  }, []);
+
   return (
     <div>
-      <Pagination data={sortedRepos} pageItem={6} />
+      {loading ? (
+        <p>Loading...</p> // แสดงข้อความโหลดข้อมูล
+      ) : (
+        <Pagination data={repos} pageItem={6} /> // ส่ง repos ที่ได้จาก API ไปยัง Pagination component
+      )}
     </div>
   );
 };
